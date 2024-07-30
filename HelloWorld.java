f92cf88fa7ee9c189d58173ddd6ecb24f2936e99
Yassar Warsi <yassar.warsi@gmail.com>
2024-07-30 22:10:06 +0530

    Fix bug 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 public static void main(String[] args) {
 System.out.println("Hello, World!");
 System.out.println(“This from Branch-1”);
+System.out.println(“This from Branch-2”);
 }
  }
